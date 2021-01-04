756d4e6eb09c1fc2cdf14c7f40ec635b6ffba08e
Srim <deamoon0801@naver.com>
2021-01-04 19:55:14 +0900

    [ADD] <RecruitNotice/index.ts> export components

---
+++ src/components/RecruitNotice/index.ts
@@ -0,0 +1,5 @@
+export { default as RecruitNotice } from "./RecruitNotice";
+export { default as FirstRecruit } from "./FirstRecruit";
+export { default as SecondRecruit } from "./SecondRecruit";
+export { default as ThirdRecruit } from "./ThirdRecruit";
+export { default as FourthRecruit } from "./FourthRecruit";
