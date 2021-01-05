22d58de138e5570582686fbb0fe4b6f505105837
Srim <deamoon0801@naver.com>
2021-01-05 23:10:06 +0900

    [ADD] <App.tsx> route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { RecruitNotice } from "./components/index";
+import { FirstRecruit, SecondRecruit, ThirdRecruit, FourthRecruit } from "./components/index";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/recruit/:id" render={() => <RecruitNotice />} />
+          <Route exact path="/recruit/1" render={() => <FirstRecruit />} />
+          <Route exact path="/recruit/2" render={() => <SecondRecruit />} />
+          <Route exact path="/recruit/3" render={() => <ThirdRecruit />} />
+          <Route exact path="/recruit/4" render={() => <FourthRecruit />} />
         </Switch>
       </Router>
     </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
-export { RecruitNotice } from "./RecruitNotice/index";
+export { FirstRecruit } from "./RecruitNotice/index"
+export { SecondRecruit } from "./RecruitNotice/index"
+export { ThirdRecruit } from "./RecruitNotice/index"
+export { FourthRecruit } from "./RecruitNotice/index"
