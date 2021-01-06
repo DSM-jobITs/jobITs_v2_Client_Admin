47ea603dca076f1718067932cbbd87d1876cb4ff
Srim <deamoon0801@naver.com>
2021-01-06 14:59:16 +0900

    [ADD] <Navbar> component

---
+++ src/components/Common/Navbar/Navbar.style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  background-color: #2f2f2f;
+  height: 94vh;
+  width: 14rem;
+  display: flex;
+  justify-content: center;
+  padding: 1.8rem 2rem;
+`;
+
+export const Container = styled.div<{ flex?: boolean }>`
+  display: ${(props) => (props.flex ? "flex" : "")};
+  justify-content: center;
+`;
+
+export const LogoImg = styled.img`
+  margin-bottom: 3.5rem;
+`;
+
+export const NavMenu = styled.h3`
+  text-align: center;
+  color: white;
+  margin-top: 0;
+  padding: 1.2rem 2rem;
+  cursor: pointer;
+  width: 14rem;
+  &:hover {
+    background-color: #349a07;
+  }
+
+  &:active {
+    background-color: #33890d;
+  }
+`;
