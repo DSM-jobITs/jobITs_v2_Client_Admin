e2439f1fc5ca458da8d1e65c45cb56b316440e77
Srim <deamoon0801@naver.com>
2021-01-06 14:58:50 +0900

    [ADD] export components

--- src/components/EmployRequest/index.ts
+++ src/components/EmployRequest/index.ts
@@ -1,1 +1,2 @@
 export { default } from "./EmployRequest";
+export { default as EmployRequestTitleBox } from "./EmployRequestTitleBox";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { FirstRecruit } from "./RecruitNotice/index"
-export { SecondRecruit } from "./RecruitNotice/index"
-export { ThirdRecruit } from "./RecruitNotice/index"
-export { FourthRecruit } from "./RecruitNotice/index"
+export { FirstRecruit } from "./RecruitNotice/index";
+export { SecondRecruit } from "./RecruitNotice/index";
+export { ThirdRecruit } from "./RecruitNotice/index";
+export { FourthRecruit } from "./RecruitNotice/index";
+export { default as EmployRequest } from "./EmployRequest/EmployRequest";
