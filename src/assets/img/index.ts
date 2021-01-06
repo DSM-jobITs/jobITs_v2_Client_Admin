78ee897caa765e8d76710a3ecf0ea081c22eabdd
Srim <deamoon0801@naver.com>
2021-01-06 18:47:45 +0900

    [ADD] arrowIcon Image

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,3 +1,5 @@
 export { default as addIcon } from "./add.svg";
 export { default as logo } from "./logo.svg";
 export { default as deleteIcon } from "./delete.svg";
+export { default as arrowBack } from "./arrowBack.svg";
+export { default as arrowForward } from "./arrowForward.svg";
