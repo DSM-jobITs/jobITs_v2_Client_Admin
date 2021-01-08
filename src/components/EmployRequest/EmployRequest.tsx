c31f83261b3795852f301fc7d2aa13286da10b1f
Srim <deamoon0801@naver.com>
2021-01-08 10:17:07 +0900

    [ADD] <EmployRequest> 임시저장 버튼 UI

--- src/components/EmployRequest/EmployRequest.tsx
+++ src/components/EmployRequest/EmployRequest.tsx
@@ -19,7 +19,7 @@ const EmployRequest = ({}: EmployRequestProps) => {
         <S.Container flex={true}>
           <S.PageNationBox>
             <S.PageNationItem>
-              <S.ArrowImg src={arrowBack}></S.ArrowImg>
+              <S.Img src={arrowBack}></S.Img>
             </S.PageNationItem>
             <S.PageNationItem>1</S.PageNationItem>
             <S.PageNationItem>2</S.PageNationItem>
@@ -27,17 +27,28 @@ const EmployRequest = ({}: EmployRequestProps) => {
             <S.PageNationItem>4</S.PageNationItem>
             <S.PageNationItem>5</S.PageNationItem>
             <S.PageNationItem last={true}>
-              <S.ArrowImg src={arrowForward}></S.ArrowImg>
+              <S.Img src={arrowForward}></S.Img>
             </S.PageNationItem>
           </S.PageNationBox>
-          <Link
-            style={{ textDecoration: "none" }}
-            to={{
-              pathname: `/first-recruit`,
-            }}
-          >
-            <S.AddButton>기업 추가</S.AddButton>
-          </Link>
+          <S.Container>
+            <Link
+              style={{ textDecoration: "none" }}
+              to={{
+                pathname: `/save`,
+              }}
+            >
+              <S.SaveButton>임시저장 보기</S.SaveButton>
+            </Link>
+
+            <Link
+              style={{ textDecoration: "none" }}
+              to={{
+                pathname: `/first-recruit`,
+              }}
+            >
+              <S.AddButton>기업 추가</S.AddButton>
+            </Link>
+          </S.Container>
         </S.Container>
       </PageTemplate>
     </>
