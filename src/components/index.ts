6cdfdd3cbd426cdf217007f8bd8d4634ba811b56
Srim <deamoon0801@naver.com>
2021-01-09 17:00:13 +0900

    [ADD] <TemporarySave> route

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { FourthRecruit } from "./RecruitNotice/index";
 export { default as EmployRequest } from "./EmployRequest/EmployRequest";
 export { default as Login } from "./Login/Login";
 export { default as DetailEmploy } from "./DetailEmploy/DetailEmploy";
+export { default as TemporarySave } from "./TemporarySave/TemporarySave";
