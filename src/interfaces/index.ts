d41328eed6c712b1fabd8894c299628e9f181ff9
Srim <deamoon0801@naver.com>
2021-03-15 23:17:01 +0900

    [ADD] GetRecruit

---
+++ src/interfaces/index.ts
@@ -0,0 +1,34 @@
+export interface entInfoObjectProps {
+  numOfWorker: number;
+  entPhone: string;
+  entSales: number;
+  address: string;
+  establishmentDate: string;
+  startTime: string;
+  endTime: string;
+}
+
+export interface workingObjectProps {
+  salary: number;
+  period: number;
+}
+
+export interface mealProps {
+  breakfast: boolean;
+  lunch: boolean;
+  dinner: boolean;
+  includeSalary: boolean;
+}
+
+export interface welfareProps {
+  fourMajor: boolean;
+  selfDevelop: boolean;
+  laptop: boolean;
+  etc: string;
+}
+
+export interface otherObjectProps {
+  personnel: number;
+  recruitPlan: boolean;
+  reception: string;
+}
--- src/lib/api/recruit.ts
+++ src/lib/api/recruit.ts
@@ -1,3 +1,5 @@
 import { getClientAccessToken } from "./client";
 
 export const onRecruit = (data: object) => getClientAccessToken().post("/recruit", data);
+
+export const getDetailRecruit = (id : string) => getClientAccessToken().get(`/recruit/detail/${id}`)
