41bfd82db2ecf108da60f3d7cfef5c72e095c4e7
Srim <deamoon0801@naver.com>
2021-03-18 16:58:36 +0900

    [ADD] <toast.ts>

---
+++ src/lib/toast.ts
@@ -0,0 +1,16 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+toast.configure();
+
+export const SuccessToast = (message: string) => {
+  toast.success(message, {
+    position: "top-right",
+    autoClose: 5000
+  });
+};
+export const ErrorToast = (message: string) => {
+  toast.error(message, {
+    position: "top-right",
+    autoClose: 5000
+  });
+};
