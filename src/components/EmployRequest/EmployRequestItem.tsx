222de6827556740afd1ee1994af99b191e3de507
Srim <deamoon0801@naver.com>
2021-03-19 22:04:08 +0900

    [ADD] <EmployRequestItem> 채용의뢰 목록 components

--- src/components/EmployRequest/EmployRequestItem.tsx
+++ src/components/EmployRequest/EmployRequestItem.tsx
@@ -2,30 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as S from "./EmployRequest.style";
 import { deleteIcon, addIcon } from "../../assets/img";
-interface EmployRequestItemProps {}
+interface EmployRequestItemProps {
+  id?: string;
+  recruitNo?: number;
+  entName?: string;
+  date?: string;
+}
 
-const EmployRequestItem = ({}: EmployRequestItemProps) => {
+const EmployRequestItem = ({ id, recruitNo, entName, date }: EmployRequestItemProps) => {
   return (
     <S.Container>
       <S.EmployBox company={true}>
         <Link
           style={{ textDecoration: "none" }}
           to={{
-            pathname: `/employ/id`,
+            pathname: `/employ/${id}`,
           }}
         >
           <S.Container company={true}>
-            <S.CompanyText>01</S.CompanyText>
-            <S.CompanyText>베어런</S.CompanyText>
+            <S.CompanyText>{recruitNo}</S.CompanyText>
+            <S.CompanyText>{entName}</S.CompanyText>
           </S.Container>
         </Link>
         <S.ImgContainer>
-          <S.TitleText date={true}>2021/01/06</S.TitleText>
+          <S.TitleText date={true}>{date}</S.TitleText>
           <S.Img src={deleteIcon} delete={true}></S.Img>
           <Link
             style={{ textDecoration: "none" }}
             to={{
-              pathname: `/edit/:id`,
+              pathname: `/edit/${id}`,
             }}
           >
             <S.Img src={addIcon}></S.Img>
