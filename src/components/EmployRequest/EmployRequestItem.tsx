44f5b686cb63a5d0bf629e556a0e712b9eaa3c42
Srim <deamoon0801@naver.com>
2021-03-20 14:51:45 +0900

    [ADD] 채용의뢰 목록 삭제

--- src/components/EmployRequest/EmployRequestItem.tsx
+++ src/components/EmployRequest/EmployRequestItem.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as S from "./EmployRequest.style";
 import { deleteIcon, addIcon } from "../../assets/img";
+import { confirmAlert } from "../../lib/sweetAlert";
 interface EmployRequestItemProps {
-  id?: string;
+  id: string;
   recruitNo?: number;
   entName?: string;
   date?: string;
+  onRemove: (id: string | undefined) => void;
 }
 
-const EmployRequestItem = ({ id, recruitNo, entName, date }: EmployRequestItemProps) => {
+const EmployRequestItem = ({ id, recruitNo, entName, date, onRemove }: EmployRequestItemProps) => {
+  const removeItem = () => {
+    confirmAlert(entName, "해당 글을 삭제하시겠습니까?", "warning", () => onRemove(id));
+  };
+
   return (
     <S.Container>
       <S.EmployBox company={true}>
@@ -26,7 +32,7 @@ const EmployRequestItem = ({ id, recruitNo, entName, date }: EmployRequestItemPr
         </Link>
         <S.ImgContainer>
           <S.TitleText date={true}>{date}</S.TitleText>
-          <S.Img src={deleteIcon} delete={true}></S.Img>
+          <S.Img src={deleteIcon} delete={true} onClick={removeItem}></S.Img>
           <Link
             style={{ textDecoration: "none" }}
             to={{
--- src/lib/api/recruit.ts
+++ src/lib/api/recruit.ts
@@ -2,4 +2,6 @@ import { getClientAccessToken } from "./client";
 
 export const onRecruit = (data: object) => getClientAccessToken().post("/recruit", data);
 
-export const getDetailRecruit = (id : string) => getClientAccessToken().get(`/recruit/detail/${id}`)
+export const getDetailRecruit = (id: string) => getClientAccessToken().get(`/recruit/detail/${id}`);
+
+export const removeRecruit = (id: string | undefined) => getClientAccessToken().delete(`/recruit/${id}`);
