059a011f2e5f98cea886dc3b4d4f5431c5e776a9
Srim <deamoon0801@naver.com>
2021-03-20 16:10:05 +0900

    [ADD] <EmployRequest> 채용의뢰 목록

--- src/lib/api/recruit.ts
+++ src/lib/api/recruit.ts
@@ -4,4 +4,6 @@ export const onRecruit = (data: object) => getClientAccessToken().post("/recruit
 
 export const getDetailRecruit = (id: string) => getClientAccessToken().get(`/recruit/detail/${id}`);
 
+export const getRecruit = (page: number) => getClientAccessToken().get(`/recruit/admin?page=${page}`);
+
 export const removeRecruit = (id: string | undefined) => getClientAccessToken().delete(`/recruit/${id}`);
