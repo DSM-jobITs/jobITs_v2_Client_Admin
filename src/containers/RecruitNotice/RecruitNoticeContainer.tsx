72d763f16b9ca75ea37b9bba974097d0cdc2ee73
Srim <deamoon0801@naver.com>
2021-04-04 23:34:30 +0900

    [ADD] <AddRecruitNotice> already data error

--- src/containers/RecruitNotice/RecruitNoticeContainer.tsx
+++ src/containers/RecruitNotice/RecruitNoticeContainer.tsx
@@ -11,23 +11,20 @@ const RecruitNoticeContainer = () => {
   const onSubmitRecruitData = (data: object) => {
     onRecruit(data)
       .then(() => {
-        console.log(data);
         SuccessToast("데이터가 성공적으로 업로드 되었습니다.");
         history.push("/employ");
       })
       .catch((err) => {
+        console.log(data);
         setError(err.response.data.message);
         setStatus(true);
-        ErrorToast("각 데이터 양식에 맞게 입력해주세요.");
+        err.response.data.message === "Already Exists Data" ? ErrorToast("중복된 기업 데이터입니다.") : ErrorToast("각 데이터 양식에 맞게 입력해주세요.");
       });
   };
 
   const onSubmitFileRecruit = (form: FormData, no: string) => {
     console.log("form" + form);
     onFileRecruit(form, no)
-      .then((res) => {
-        console.log(res);
-      })
       .catch((err) => {
         console.log(err);
         ErrorToast("파일을 다시 확번 확인해주세요.");
