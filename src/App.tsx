e43bbffc8c72ae4625d87a79e179455a3c933ea3
Srim <deamoon0801@naver.com>
2021-04-07 09:46:01 +0900

    [ADD] auth check

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { RecruitNotice, EmployRequest, Login, DetailEmploy, TemporarySave } from "./containers/index";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { RecruitNotice, EmployRequest, Login, DetailEmploy, TemporarySave, UpdateRecruit } from "./containers/index";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
-          <Route exact path="/recruit" render={() => <RecruitNotice />} />
-          <Route exact path="/employ" render={() => <EmployRequest />} />
-          <Route exact path="/employ/:id" render={() => <DetailEmploy />} />
           <Route exact path="/" render={() => <Login />} />
+          <Route exact path="/recruit">
+            {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <RecruitNotice />}
+          </Route>
+          <Route exact path="/employ">
+            {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <EmployRequest />}
+          </Route>
+          <Route exact path="/employ/:id">
+            {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <DetailEmploy />}
+          </Route>
           <Route exact path="/save" render={() => <TemporarySave />} />
+          <Route exact path="/edit/:id">
+            {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <UpdateRecruit />}
+          </Route>
         </Switch>
       </Router>
     </div>
