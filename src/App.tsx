c9e8d5916e23caaafb064febfebdd87dcca2f3dd
Srim <deamoon0801@naver.com>
2021-04-15 16:15:39 +0900

    [ADD] <EmployPage> auth middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import { RecruitNotice, EmployRequest, Login, DetailEmploy, TemporarySave, UpdateRecruit } from "./containers/index";
+import { RecruitNotice, EmployRequest, Login, DetailEmploy, UpdateRecruit } from "./containers/index";
 
 function App() {
   return (
@@ -11,13 +11,10 @@ function App() {
           <Route exact path="/recruit">
             {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <RecruitNotice />}
           </Route>
-          <Route exact path="/employ">
-            {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <EmployRequest />}
-          </Route>
+          <Route exact path="/employ" render={() => <EmployRequest />} />
           <Route exact path="/employ/:id">
             {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <DetailEmploy />}
           </Route>
-          <Route exact path="/save" render={() => <TemporarySave />} />
           <Route exact path="/edit/:id">
             {!localStorage.getItem("accessToken") ? <Redirect to="/" /> : <UpdateRecruit />}
           </Route>
--- src/containers/Employ/EmployPageContainer.tsx
+++ src/containers/Employ/EmployPageContainer.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { useHistory } from "react-router-dom";
 import { getRecruit, removeRecruit } from "../../lib/api/recruit";
 import EmployRequest from "../../components/EmployRequest/EmployRequest";
 import { WarningToast, ErrorToast } from "../../lib/toast";
 
 const EmployPageContainer = () => {
+  const history = useHistory();
   const [page, setPage] = useState(0);
   const [recruitList, setRecruitList] = useState([]);
   const [text, setText] = useState(false);
@@ -32,11 +34,15 @@ const EmployPageContainer = () => {
       ? WarningToast("가장 최근 페이지입니다.")
       : getRecruit(page === 0 ? 0 : page)
           .then((res) => {
+            console.log(res.status);
             setRecruitList(res.data.response);
             res.data.response.length < 1 ? setText(true) : setText(false);
           })
-          .catch(() => {
-            ErrorToast("데이터를 불러오는 데에 실패했습니다.");
+          .catch((err) => {
+            if (err.response.status === 403) {
+              history.push("/");
+              WarningToast("로그인 후 이용해주세요.");
+            } else ErrorToast("데이터를 불러오는 데에 실패했습니다.");
           });
   }, [page]);
 
