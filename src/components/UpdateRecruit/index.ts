cd258ed3b68b10ba69a1c3986571c5a2dd1940c0
Srim <deamoon0801@naver.com>
2021-04-29 19:38:11 +0900

    [ADD] <UpdateRecruit/index.ts> export components

---
+++ src/components/UpdateRecruit/index.ts
@@ -0,0 +1,8 @@
+export { default as WelfareInfo } from "./WelfareInfo";
+export { default as InitDataInfo } from "./InitDataInfo";
+export { default as ManagerInfo } from "./ManagerInfo";
+export { default as QualificationInfo } from "./QualificationInfo";
+export { default as RecruitPlanInfo } from "./RecruitPlanInfo";
+export { default as WorkingDataInfo } from "./WorkingDataInfo";
+export { default as UpdateRecruit } from "./UpdateRecruit";
+export { default as EntInfo } from "./EntInfo";
